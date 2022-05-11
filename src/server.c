48998e4fdbd9aa578a47b88c4326fa1e47c41ed1
sandeepr03 <103075548+sandeepr03@users.noreply.github.com>
2022-05-11 02:58:15 -0700

    Update server.c
    Made working shuffle and assign functions.  Can be checked by looking at the unique suit and rank of each card printed out when program is run. Every card has a different suit and rank showing none of them are the same and are assigned properly.

--- src/server.c
+++ src/server.c
@@ -2,12 +2,29 @@
 #include <stdio.h>
 
 int main(){
-DECK deck = INIT();
-CARD c1 = {SPADES, 3};
-CARD c2 = {HEARTS, 4};
-PLAYER p1;
-p1.card1 = c1;
-p1.card2 = c2;
-printf("%d %d", p1.card1.rank, p1.card2.suit);
+  DECK deck = INIT();
 
+  /*CARD c1 = {SPADES, 3};
+  CARD c2 = {HEARTS, 4};
+  PLAYER p1;
+  p1.card1 = c1;
+  p1.card2 = c2;
+  printf("%d %d", p1.card1.rank, p1.card2.suit);*/
+
+  PLAYER player[4];
+  deck = ShuffleCards (deck);
+
+  deck = AssignCards(player, deck);
+  
+  
+  for(int i = 0;i<=4;i++) //CHECKING THE CARDS EACH PLAYER HAS
+  {
+      printf("%d Player suit is %d and rank is %d\n", i , player[i].card1.suit, player[i].card1.rank);
+      printf("%d Player suit is %d and rank is %d\n", i , player[i].card2.suit, player[i].card2.rank);
+  }
+
+  for(int i = 0;i<=51;i++) //CHECKING THE REMAINING CARDS IN THE DECK
+  {
+      printf("Deck %d suit is %d and rank is %d\n", i , deck.cards[i].suit, deck.cards[i].rank);
+  }
 }
