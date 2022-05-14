8570bb6c585412803fbe7e51fd470ca9cfe3b4b1
sandeepr03 <103075548+sandeepr03@users.noreply.github.com>
2022-05-13 19:30:22 -0700

    Update deck.c
    AssignCards: This function now assigns cards to all players, and also assigns small blind and big blind to the players if the gamestate is in preflop. If the gamestate is not preflop, the function moves the current small and big blinds to the next players. 
    PERFLOP1: This is a new function, which runs the game. The function keeps running until all the bids are equal. when the program will stop and print out the cards and bets of each player. If a player folds, it will not prompt that player to input again. 
    EQUALBIDS: This function checks if the bids from all players are equal. It ignores the bets from players who have already folded. It only works if player 0 did not fold, for now, I can change that soon.
    The preflop stage seems to be working well now  although there are a few things that need to be changed.

--- src/deck.c
+++ src/deck.c
@@ -38,27 +38,146 @@ DECK ShuffleCards (DECK deck)
     return shuffle;
 }
 
-
-DECK AssignCards (PLAYER player[], DECK deck)
+void AssignCards (PLAYER player[], DECK deck, int n, GAMESTATE G)
 {
     int i,x;
+    PLAYER P[n+1]; //Temporary variable
+
     for (i = 0, x = 0; i <= 4; i++, x=x+2)
     {
         player[i].card1 = deck.cards[x]; //Assign 1st Card to Player[i]
-        player[i].card2 = deck.cards[x+1]; //Assign 2nd Card ro Player[i]
+        player[i].card2 = deck.cards[x+1]; //Assign 2nd Card to Player[i]
     }
 
-    for(int a = 0; a<= 51 - 10;a++)
+    for(int a = 0; a<= 41 - 10;a++)
     {
-        deck.cards[a] = deck.cards[a+10]; //Changing the deck by removing first 10 cards
+        deck.cards[a] = deck.cards[a+10]; //Changing the deck by removing the assigned cards
+    }
+    
+    for (int a = 0; a <= n ; a++) //Making P equal to Player as a temporary variable
+    {
+        P[a] = player[a];
+    }
+    
+    if (G.stage = PREFLOP)
+    {
+        player[0].role = 0; //Assigning small blind to player[0]
+        player[0].Bid = 5; //Assigning default bid on small blind
+        player[1].role = 1; //Assigning big blind to player[1]
+        player[1].Bid = 10; //Assigning default bid on big blind
+        for (int a = 2; a <= n ; a++)
+        {
+            player[a].role = 2; //Assigning other players ar normal players
+            player[a].Bid = 0; //Assigning 0 bid for non-Blind player
+        }
     }
 
-    return deck;
+    else 
+    {
+        for (int a = 0; a <= n ; a++) //Moving the player roles[Blinds] and assigning 
+        {                             //default bids on binds, at the start of the round   
+            if (a > 0)
+          {
+                player[a].role = P[a-1].role;
+            }
+
+            else 
+            {
+                player[a].role = P[n].role;
+            }
+        }
+    }
+    //return deck;
+}
+
+void PREFLOP1(PLAYER player[], DECK deck, int n)
+{
+    int option = 0;
+    int raise = 0;
+    int min_bid = 10;
+    int x = 0;
+
+    while (1)
+    {
+        if(EQUALBIDS(player, n) == 1) //Bidding goes on untill everyone has equal bids
+        {
+            break;
+        }
+        
+        if (player[x].action == FOLD) //If a player folded, we do not ask his input again
+        {
+            x++;
+            if(x == n+1)
+            {
+                x = 0;
+            }
+            continue;
+        }
+
+        printf("Player %d, choose 1  for call, 2 for raise, 3 for fold: ",x);
+        scanf("%d",&option);
+
+        switch (option)
+        {
+            case 1 : //Calling the Bid
+                player[x].action = CALL;
+                player[x].Bid = min_bid;
+                break;
+
+            case 2 : //Raising the bid
+                player[x].action = RAISE;
+                printf("Enter the raise amount: ");
+                scanf("%d",&raise);
+                min_bid = min_bid + raise;
+                player[x].Bid = min_bid;
+                break;
+
+            case 3: //Folding
+                player[x].action = FOLD;
+                break;
+        }
+        x++;
+        if(x == n+1) //To keep the loop going within the bounds
+        {
+            x = 0;
+        }
+
+    } 
 }
 
 
+int EQUALBIDS(PLAYER player[], int n)
+//This works only if player[0] did not fold (For now)
+{
+    if (player[0].action != FOLD) 
+    {
+        for(int a = 1; a <= n ; a++)
+        {
+            if (player[a].action == FOLD) //Not considering the bid of a folded player
+                {
+                   continue;
+                }
 
+            if (player[0].Bid == player[a].Bid) //Checking if all bids are equal
+                {
+                if (a == n)
+                    {
+                       return 1; //Returning 1 if all bids are equal
+                    }
 
+                else
+                    {
+                        continue;
+                    }
+                }
+
+            else
+            {
+                return 0; //Returning 0 if even one of the bids are not equal
+            }
+        }
+    }
+}
 
 
 void swap(int *a, int *b) {
@@ -75,3 +194,4 @@ void random(int A[], int B) {
         swap(&A[i], &A[j]); //Swap two elements in the array randomly
     }
 }
+
