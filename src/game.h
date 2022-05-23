798f1a4f7dffef14bb42ea7dc98963961ead2bc7
sandeepr03 <103075548+sandeepr03@users.noreply.github.com>
2022-05-23 16:34:52 -0700

    Update game.h

--- src/game.h
+++ src/game.h
@@ -5,5 +5,21 @@ GAMESTATE PREFLOP1(GAMESTATE game); //Bidding in PreFlop
 GAMESTATE FLOP1(GAMESTATE game);
 int EQUALBIDS(GAMESTATE game); //Checking if all bids are equal to proceed further
 
+PLAYER Sequence_Winner (GAMESTATE game);
+int CheckPlayer (GAMESTATE game);
+
+int IsRoyalFlush (GAMESTATE game, int PlayerNumber);
+int IsStraightFlush(GAMESTATE game, int PlayerNumber);
+int IsFourofaKind (GAMESTATE game, int PlayerNumber);
+int IsFullHouse(GAMESTATE game, int PlayerNumber);
+int IsFlush(GAMESTATE game, int PlayerNumber);
+int IsStraight (GAMESTATE game, int PlayerNumber);
+int IsThreeofaKind (GAMESTATE game, int PlayerNumber);
+int IsTwoPair (GAMESTATE game, int PlayerNumber);
+int IsOnePair (GAMESTATE game, int PlayerNumber);
+
+int LargestinArray (int A, int B);
+DECK SortbyRank(DECK D);
+
 void swap(int *a, int *b);
 void random(int arr[], int n);
