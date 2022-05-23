7386e43c9bc75fd74350a4afd87af6eef470cea5
sandeepr03 <103075548+sandeepr03@users.noreply.github.com>
2022-05-23 16:33:56 -0700

    Update game.c
    Added new functions: CheckPlayer() checks a player's deck and assigns him a number according to his sequence and matches.
    Sequence_Winner() Compares the ranking of different players and assigns a winner
    The IsSequence() Functions are still to be implemented. IsFlush works for now.
    SortByRank and LargestinArray functions were made for isFlush to work. SortbyRank() Takes a player 's cards and arranges the cards in ascending order of rank with the community cards.

--- src/game.c
+++ src/game.c
@@ -124,11 +124,168 @@ int EQUALBIDS(GAMESTATE game)
 }
 
 
+
 void swap(int *a, int *b) {
     int X = *a;
     *a = *b;
     *b = X;
 }
+
+
+DECK SortbyRank(DECK D)
+{
+    CARD X;
+    int i, j;
+    for(i=0;i<7;i++)
+    {
+        for (j=0 ; j<7-i-1; j++)
+        {
+            if(D.cards[j].rank > D.cards[j+1].rank)
+            {
+                X = D.cards[j];
+                D.cards[j] = D.cards[j+1];
+                D.cards[j+1] = X;
+            }
+        }
+        
+    }
+    for (int i=0;i<7;i++)
+    {
+        printf("Card %d Rank = %d\n", i, D.cards[i].rank);
+    }
+    return D;
+}
+
+int IsRoyalFlush (GAMESTATE game, int PlayerNumber)
+{return 0;}
+int IsStraightFlush(GAMESTATE game, int PlayerNumber){return 0;}
+int IsFourofaKind (GAMESTATE game, int PlayerNumber){return 0;}
+int IsFullHouse(GAMESTATE game, int PlayerNumber){return 0;}
+int IsStraight (GAMESTATE game, int PlayerNumber){return 0;}
+int IsThreeofaKind (GAMESTATE game, int PlayerNumber){return 0;}
+int IsTwoPair (GAMESTATE game, int PlayerNumber){return 0;}
+int IsOnePair (GAMESTATE game, int PlayerNumber){return 0;}
+
+int IsFlush(GAMESTATE game, int PlayerNumber)
+{
+    DECK NewDeck;
+    int i, a, b, c, d, e, f, g;
+    for (i = 0; i< 5; i++)
+    {
+        NewDeck.cards[i] = game.communityCards.cards[i];
+    }
+    NewDeck.cards[5] = game.players[PlayerNumber].card1;
+
+    NewDeck.cards[6] = game.players[PlayerNumber].card2;
+
+    NewDeck = SortbyRank(NewDeck);
+
+    a = NewDeck.cards[0].rank;
+    b = NewDeck.cards[1].rank;
+    c = NewDeck.cards[2].rank;
+    d = NewDeck.cards[3].rank;
+    e = NewDeck.cards[4].rank;
+    f = NewDeck.cards[5].rank;
+    g = NewDeck.cards[6].rank;
+
+    if ((e == d+1 && d == c+1 && c == b+1 && b == a+1) || (f == e+1 && e == d+1 && d == c+1 && c == b+1) || (g == f+1 && f == e+1 && e == d+1 && d == c+1))
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
+    return 0;
+} 
+
+int CheckPlayer (GAMESTATE game, int PlayerNumber)
+{
+    int priority;
+
+    if (IsRoyalFlush(game,PlayerNumber) == 1)
+    {
+        priority = 10;
+    }
+    
+    else if (IsStraightFlush(game,PlayerNumber) == 1)
+    {
+        priority = 9;
+    }
+
+    else if (IsFourofaKind (game,PlayerNumber) == 1)
+    {
+        priority = 8;
+    }
+
+    else if (IsFullHouse(game,PlayerNumber) == 1)
+    {
+        priority = 7;
+    }
+    
+    else if (IsFlush (game,PlayerNumber) == 1)
+    {
+        priority = 6;
+    }
+
+    else if (IsStraight (game,PlayerNumber) == 1)
+    {
+        priority = 5;
+    }
+
+    else if (IsThreeofaKind (game,PlayerNumber) == 1)
+    {
+        priority = 4;
+    }
+
+    else if (IsTwoPair (game,PlayerNumber) ==1)
+    {
+        priority = 3;
+    }
+
+    else if (IsOnePair (game,PlayerNumber) ==1)
+    {
+        priority = 2;
+    }
+
+    else 
+    {
+        priority = 1;
+    }
+
+    return priority;
+}
+
+int LargestinArray (int A[], int B)
+{
+    int i;
+    int max = 1;
+ 
+    for (i = 0; i < B; i++)
+        if (A[i] > max)
+        {
+            max = i;
+        }
+            
+    return max;
+}
+
+PLAYER Sequence_Winner (GAMESTATE game)
+{
+    int A[9], i;
+    for (i = 0; i < game.numberPlayers; i++)
+    {
+        //if(game.players[i].action != FOLD)
+        {
+            A[i] = CheckPlayer(game, i);
+        }
+    }
+    int Winner = LargestinArray (A, i);
+
+    return game.players[Winner];
+}
+
+
 
 void random(int A[], int B) {
     srand(time(NULL));
@@ -138,3 +295,4 @@ void random(int A[], int B) {
         swap(&A[i], &A[j]); //Swap two elements in the array randomly
     }
 }
+
