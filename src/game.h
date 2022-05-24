101eb7c5761c427b4d7d44d77253d0c7b3b6d700
sandeepr03 <103075548+sandeepr03@users.noreply.github.com>
2022-05-24 12:40:05 -0700

    Update game.h
    Fixed errors

--- src/game.h
+++ src/game.h
@@ -6,7 +6,7 @@ GAMESTATE FLOP1(GAMESTATE game);
 int EQUALBIDS(GAMESTATE game); //Checking if all bids are equal to proceed further
 
 PLAYER Sequence_Winner (GAMESTATE game);
-int CheckPlayer (GAMESTATE game);
+int CheckPlayer (GAMESTATE game, int PlayerNumber);
 
 int IsRoyalFlush (GAMESTATE game, int PlayerNumber);
 int IsStraightFlush(GAMESTATE game, int PlayerNumber);
@@ -18,7 +18,7 @@ int IsThreeofaKind (GAMESTATE game, int PlayerNumber);
 int IsTwoPair (GAMESTATE game, int PlayerNumber);
 int IsOnePair (GAMESTATE game, int PlayerNumber);
 
-int LargestinArray (int A, int B);
+int LargestinArray (int A[], int B);
 DECK SortbyRank(DECK D);
 DECK SortbySuit (DECK D);
 
